4ad40ff633c67abd848079843c5a29edd3ebd158
Khondokar Mehraz <119512297+itz-mehraz@users.noreply.github.com>
2023-10-10 14:54:22 +0600

    Create Task10-ObserverPattern.java
    The Observer Pattern is a behavioral design pattern that defines a one-to-many relationship between objects. One object (the subject) maintains a list of dependent objects (observers) that are notified of changes to the subject's state
    
    In this example:
    
    We define an Observer interface with an update method.
    We implement a concrete observer class ConcreteObserver that implements the Observer interface.
    We define a Subject interface with registerObserver, removeObserver, and notifyObservers methods.
    We implement a concrete subject class ConcreteSubject that maintains a list of observers and notifies them when its state changes.
    In the client code, we create a ConcreteSubject instance and two ConcreteObserver instances. We register these observers with the subject and then change the subject's state. Observers are notified of state changes, and they update accordingly.
    We also demonstrate how to unregister an observer, and when the state changes again, only the remaining observer is notified.
    This example illustrates the Observer Pattern, where the subject maintains a list of observers and notifies them when its state changes.

---
+++ Task10-ObserverPattern.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface Observer {
+    void update(String message);
+}
+
+class ConcreteObserver implements Observer {
+    private String name;
+
+    public ConcreteObserver(String name) {
+        this.name = name;
+    }
+
+    public void update(String message) {
+        System.out.println(name + " received message: " + message);
+    }
+}
+
+interface Subject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers(String message);
+}
+
+class ConcreteSubject implements Subject {
+    private List<Observer> observers = new ArrayList<>();
+    private String state;
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
+    public void setState(String state) {
+        this.state = state;
+        notifyObservers("State changed to: " + state);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ConcreteSubject subject = new ConcreteSubject();
+
+        Observer observer1 = new ConcreteObserver("Observer 1");
+        Observer observer2 = new ConcreteObserver("Observer 2");
+
+        subject.registerObserver(observer1);
+        subject.registerObserver(observer2);
+
+        subject.setState("New State 1");
+        subject.setState("New State 2");
+
+        subject.removeObserver(observer1);
+
+        subject.setState("New State 3");
+    }
+}
